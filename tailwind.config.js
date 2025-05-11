590c9edbdef3c52f5a182d31466470e832ecaf92
Devaaldo <akbarprdn2512@gmail.com>
2025-05-12 06:23:36 +0700

    first configuration

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#9333ea', // Warna ungu primary
+          dark: '#7e22ce',
+          light: '#a855f7',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
